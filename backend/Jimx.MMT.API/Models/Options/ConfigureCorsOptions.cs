571ef844ededc8f634c9592be4378b0df4b74239
Artur Usmanov <arnystreamer@gmail.com>
2024-11-01 16:18:21 +0100

    infrastructure tuning

---
+++ backend/Jimx.MMT.API/Models/Options/ConfigureCorsOptions.cs
@@ -0,0 +1,31 @@
+﻿using Jimx.MMT.API.Services;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.Extensions.Options;
+
+namespace Jimx.MMT.API.Models.Options
+{
+	public class ConfigureCorsOptions : IConfigureNamedOptions<CorsOptions>
+	{
+		private readonly SettingsProvider _settingsProvider;
+
+		public ConfigureCorsOptions(SettingsProvider settingsProvider)
+		{
+			_settingsProvider = settingsProvider;
+		}
+
+		public void Configure(string? name, CorsOptions options)
+		{
+			Configure(options);
+		}
+
+		public void Configure(CorsOptions options)
+		{
+			options.AddPolicy("Frontend",
+			policy =>
+			{
+				policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+				policy.WithOrigins(_settingsProvider.FrontendUrl).AllowAnyOrigin().AllowAnyHeader();
+			});
+		}
+	}
+}
